69c4361da533a5052fe207848999e4a46f827542
Bárbara Boechat <barbara_belize@hotmail.com>
2019-05-29 23:40:37 -0300

    +geosearch funcionando

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -16,8 +16,24 @@
             $('.scrolling-navbar').removeClass('top-nav-collapse');
         }
     });
+
+
     //Mapa Atualmente
     var mapAtual = L.map('mapAtual', {
+        center: [-21.1312539, -44.2535548],
+        zoom: 13
+      });
+
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoibmllaGF1cyIsImEiOiJjancxOG5qeDcwNGZlNDVvNXEyOGpwaWEzIn0.wxetZ_BKGpdp4JvOqttX4A'
+      }).addTo(mapAtual);
+
+      //Mapa with searchControl
+    //  var map = L.map('map').setView( [-21.1312539, -44.2535548],13);
+     var map = L.map('map', {
         center: [-21.1312539, -44.2535548],
         zoom: 13
       });
@@ -26,9 +42,43 @@
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoibmllaGF1cyIsImEiOiJjancxOG5qeDcwNGZlNDVvNXEyOGpwaWEzIn0.wxetZ_BKGpdp4JvOqttX4A'
-      }).addTo(mapAtual);
+      }).addTo(map);
 
+      var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+      //Adding layergroups to searchControl
+      var results = L.layerGroup().addTo(map);
+      var results2 = L.layerGroup().addTo(mapAtual);
+      var popup= L.popup();
+
+      var myIcon = new L.Icon({
+          iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+          shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+          shadowSize: [41, 41]
+          });
+
+      searchControl.on('results', function(data){
+        results.clearLayers();
+        for(var i = data.results.length - 1; i >= 0; i--){
+          results.addLayer(L.marker(data.results[i].latlng,{icon: myIcon})
+          .bindPopup($("#msg_subject").val() + " \n" + setPopUp()).openPopup());
+          popup
+            .setLatLng(data.results[i].latlng)
+            .setContent(data.results[i].latlng.toString()
+            );
+
+            results2.addLayer(L.marker(data.results[i].latlng,{icon: myIcon})
+            .bindPopup($("#msg_subject").val()  + " \n" + setPopUp()).openPopup());
+            popup
+              .setLatLng(data.results[i].latlng)
+              .setContent(data.results[i].latlng.toString()
+              );
+        }
+      })
     //Mapa denuncia
+/*
     var map = L.map('map', {
         center: [-21.1312539, -44.2535548],
         zoom: 13
@@ -47,6 +97,7 @@
           popupAnchor: [1, -34],
           shadowSize: [41, 41]
           });
+
       var marker, popupText, popup= L.popup();
 
       function onMapClick(e) {
@@ -55,14 +106,15 @@
         popup
           .setLatLng(e.latlng)
           .setContent(e.latlng.toString())
-
       }
       map.on('click', onMapClick);
+
+      marker = L.marker([-21.1254637, -44.2476314],{icon: myIcon}).addTo(map), L.marker([-21.1254637, -44.2476314],{icon: myIcon}).addTo(mapAtual);
     // one page navigation
     $('.navbar-nav').onePageNav({
       currentClass: 'active'
     });
-
+*/
     /* Auto Close Responsive Navbar on Click
     ========================================================*/
     function close_toggle() {
---
+++ public/js/denuncie.js
@@ -0,0 +1,11 @@
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+
+const form = document.querySelector('form');
+const input = form.querySelector('input[type="text"]');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const results = await provider.search({ query: input.value });
+  console.log(results); // » [{}, {}, {}, ...]
+});
